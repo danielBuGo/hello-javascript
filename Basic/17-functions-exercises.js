6b45d17291999aad815a516b10a71f4da5365e87
danielBuGo <danielbuilesgomez@gmail.com>
2025-03-07 17:04:53 -0500

    doing the functions exercises

--- Basic/17-functions-exercises.js
+++ Basic/17-functions-exercises.js
@@ -5,22 +5,66 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=14146
 
 // NOTA: Explora diferentes sintaxis de funciones para resolver los ejercicios
 
-// 1. Crea una función que reciba dos números y devuelva su suma
+console.log("1. Crea una función que reciba dos números y devuelva su suma") 
+//Funcion simple:
+function add(a=0, b=0){
+    console.log(a+b)
+}
+add(10,10)
+//Funcion Anonima
+let anonymAdd = function (a=0, b=0){
+    console.log(a+b)
+}
+anonymAdd(5,8)
+//arrow function
+let arrowAdd = (a=0,b=0) => console.log(a+b)
+arrowAdd(8,8)
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
-// 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
+console.log("2. Crea una función que reciba un array de números y devuelva el mayor de ellos")
+const numbers = [7,14,21,28]
+function theBigOne (array) {
+         
+    for (let i = 0; i < numbers.length; i++) {
+        let max = 0; 
+       
+       if(max > numbers[i]){
+       max = numbers[i]            
+       }
+       return max        
+    }
+}
+const data = theBigOne(numbers)
+console.log(data)
+theBigOne()
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++")
+/**function theChoosenOne (){
+    const numbers = [500, 6, 245, 76, 89]
+    let max = 0
+    for (const i of numbers) {
+        console.log(i)
+    } 
+
+}**/
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
 // 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
-
-// 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
-
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+console.log("4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas")
+let words = "Give it back in Uppers"
+let toUpper = function (string) {
+   console.log(string.toUpperCase())
+}
+toUpper(words)
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
-
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 // 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
-
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 // 7. Crea una función que reciba un array de números y devuelva la suma de todos los números pares
-
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 // 8. Crea una función que reciba un array de números y devuelva un nuevo array con cada número elevado al cuadrado
-
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 // 9. Crea una función que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso
-
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 // 10. Crea una función que calcule el factorial de un número dado
