99a8d59ed6fa00efa16b5cdeb9cd4b1700e12785
danielBuGo <danielbuilesgomez@gmail.com>
2025-03-15 17:29:16 -0500

    Update 22-classes.js
    finished

--- Basic/22-classes.js
+++ Basic/22-classes.js
@@ -4,190 +4,169 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=16864
 */
 
 // Clases
+let person = {
+    name: "Mariana",
+    age: 15,
+    alias: "Mari"
+};
 
-class Person {
-
+class Person {//Las clases inician con la primera leta  en mayus
     constructor(name, age, alias) {
         this.name = name
         this.age = age
         this.alias = alias
     }
-
-}
+};
+let subject = new Person ("Manuela", 13, "Mamu") //con el new se llama la clase
+let subject2 = new Person("Lina", 36, "reina")
+console.log(subject)
+console.log(subject2)
+console.log(typeof subject)
 
 // Sintaxis
-
-let person = new Person("Brais", 37, "MoureDev")
-let person2 = new Person("Brais", 37, "MoureDev")
-
-console.log(person)
-console.log(person2)
-
-console.log(typeof person)
-
 // Valores por defecto
 
 class DefaultPerson {
-
-    constructor(name = "Sin nombre", age = 0, alias = "Sin alias") {
+    constructor(name = "Sin nombre", age = 0, alias = "sin alias"){
         this.name = name
         this.age = age
         this.alias = alias
     }
-
-}
-
-let person3 = new DefaultPerson("Brais", 37)
-
-console.log(person3)
-
-// Acceso a propiedades
-
-console.log(person3.alias)
-console.log(person3["alias"])
-
-person3.alias = "MoureDev"
-
-console.log(person3.alias)
+};
+let subject3 = new DefaultPerson("madabeski", 50) //de esta manera se deben agregar propiedades en orden
+console.log(subject3)
+//Acceso a propiedades
+console.log("+++ Acceso a propiedades +++")
+console.log(subject3.alias)
+console.log(subject2.age)
+console.log(subject.name)
+console.log("+++ Agregar valores +++")
+subject3.alias = "Mada" // de esta manera se asigna proipedades a cada variable
+console.log(subject3)
 
 // Funciones en clases
-
-class PersonWithMethod {
-
+class PersonWithMetod {//Las clases inician con la primera leta  en mayus
     constructor(name, age, alias) {
         this.name = name
         this.age = age
         this.alias = alias
     }
-
-    walk() {
-        console.log("La persona camina.")
+    walk(){
+         console.log("the person is walking")
     }
+};
+let subject4 = new PersonWithMetod("Jorge", 44, "Angulo")
+console.log("__________________________________")
+console.log(subject4)
+subject4.walk()
 
-}
+class CarWithMetod {
+    constructor(brand, model, year){
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
+    carIsON(){
+        console.log("The car is turning on")
+    }
+};
+let car1 = new CarWithMetod("GMC", "Hammer", 2020)
+console.log(car1)
+car1.carIsON()
+car1.brand = "ferrari"
+console.log(car1)
 
-let person4 = new PersonWithMethod("Brais", 37, "MoureDev")
-person4.walk()
-
-// Propiedades privadas
-
-class PrivatePerson {
-
-    #bank
-
-    constructor(name, age, alias, bank) {
+console.log("+++ Propiedades privadas +++")
+class PrivatePerosn {
+    #bank //Asi se especifica que un dato es privado
+    constructor(name, age, alias, bank){
         this.name = name
-        this.age = age
         this.alias = alias
-        this.#bank = bank
+        this.age = age
+        this.#bank = bank //Dato privado
     }
-
-    pay() {
+    pay(){
         this.#bank
+        console.log("Function ")
     }
-
-}
-
-let person5 = new PrivatePerson("Brais", 37, "MoureDev", "IBAN123456789")
-
+};
+let subject5 = new PrivatePerosn("Julian", 46, "divino", "Ju345879")
+console.log(subject5) //Se muestran todos los datos menos los privados
+subject5.pay()
+subject5.bank = "this is a different bank"
+console.log(subject5)
 // No podemos acceder
 // console.log(person5.bank) 
 // person5.bank = "new IBAN123456789" // bank no es #bank
-
-console.log(person5)
-
-// Getters y Setters
-
-class GetSetPerson {
-
+console.log("+++ Getter and Setter +++")
+class GetPerson {
     #name
     #age
     #alias
+    #Id
     #bank
-
-    constructor(name, age, alias, bank) {
+    constructor(name, age, alias, Id, bank){
         this.#name = name
         this.#age = age
         this.#alias = alias
+        this.#Id = Id
         this.#bank = bank
     }
-
-    get name() {
-        return this.#name
+    get name(){ //Por medio de un Get podemos acceder al nombre
+        return this.#name       
     }
-
-    set bank(bank) {
-        this.#bank = bank
-    }
-
-}
-
-person6 = new GetSetPerson("Brais", 37, "MoureDev", "IBAN123456789")
-
-console.log(person6)
-console.log(person6.name)
-
-person6.bank = "new IBAN123456789"
+    set name(newBank){
+        this.#name = newBank //Se uede modificar el dato privado por medio del set
+    } 
+};
+let subject6 = new GetPerson("Nicolas", 59, "Nico", 1039448690, "bancolombia")
+console.log(subject6)
+console.log(subject6.name)
+subject6.name = "otherbank"
+console.log(subject6.name)
 
 /*
 Clase 38 - Herencia de clases
 Vídeo: https://youtu.be/1glVfFxj8a4?t=17999
 */
-
-// Herencia
-
+console.log("+++ Herencia de clases +++")
 class Animal {
-
-    constructor(name) {
-        this.name = name
+    constructor(nombre){
+        this.nombre = nombre
     }
-
-    sound() {
-        console.log("El animal emite un sonido genérico")
+    sound (){
+        console.log("Make a sound")
     }
-
-}
-
+};
 class Dog extends Animal {
 
-    sound() {
-        console.log("Guau!")
+    run(){
+        console.log("the dog is running")
     }
+};
+let mydog = new Dog("Bella")
+console.log(mydog)
+mydog.sound()
+mydog.run()
 
-    run() {
-        console.log("El perro corre")
-    }
-
-}
-
-class Fish extends Animal {
-
-    constructor(name, size) {
+class Bird extends Animal {
+    constructor (name, size){
         super(name)
-        this.size = size
+        this.size =size
     }
-
-    swim() {
-        console.log("El pez nada")
+    fly(){
+        console.log("The bird is flying")
     }
-
-}
-
-let myDog = new Dog("MoureDog")
-myDog.run()
-myDog.sound()
-
-let myFish = new Fish("MoureFish", 10)
-myFish.swim()
-myFish.sound()
+};
+let myBird = new Bird("Gallinazi", 10)
+console.log(myBird) 
+myBird.sound()
+myBird.fly()
 
 // Métodos estáticos
-
-class MathOperations {
-
-    static sum(a, b) {
+class MathOperators {
+    static sum(a, b){ //El static nos permite llamar directamente la funcion
         return a + b
     }
-}
-
-console.log(MathOperations.sum(5, 10))
+};
+console.log(MathOperators.sum(5,5))
