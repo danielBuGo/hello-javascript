b2ce5a00e7741d907da01da50cae1c5e34664c9a
Daniel Builes Gómez <danielbuilesgomez@gmail.com>
2025-03-17 19:00:44 -0500

    starting error-handling  exercises

--- 24-error-handling.js
+++ 24-error-handling.js
@@ -4,47 +4,84 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=18751
 */
 
 // Excepción
+/** Si cometemos un error en nuestro codigo, el usuario simplemente verá como se para
+ * la app. Nosotros por terminal podríamos verlo pero el usuario solo vería la pantalla 
+ * tildada:
+ * Ejemplo
+ */
+
+//console.log(myObject.name) //no hemos creado el objeto correctamente
 
 // Produce una excepción
+let myObject
+// Captura de errores con Try-Catch
+try {
+    console.log(myObject)
+    console.log("✔ It was executed without errors")
 
-// console.log(myObject.email)
-
-// Captura de errores
-
-// try-catch
-
-
-
-// Captura del error
-
-
-
-// finally
+} catch(error){
+    console.log("x  An error has occurred: ", error.message)
+} finally {
+    console.log("✔ this code will execute always 😊")
+}
 
 
-
-// No está soportado
-// try {
-//     console.log(myObject.email)
-// } finally {
-//     console.log("Este código se ejecuta siempre")
-// }
 
 // Lanzamiento de errores
-
 // throw
 
-// throw new Error("Se ha producido un error")
-
-
-
-
-
+function sum(a,b){
+    if(typeof a !== "number" || typeof b !== "number"){
+        throw new TypeError("You must use numbers")
+    }
+    if(!Number.isInteger(a) || !Number.isInteger(b)){
+        throw new Error("Only must use integer numbers")
+    } if(a == 0 || b == 0){
+        throw new MyCustomError("you are trying to add 0: ", a, b)
+    }
+    return a + b
+    
+};
+try{
+    console.log(sum(5,"9.8"))
+    //console.log(sum("5",9))
+    console.log(sum(5,9))
+} catch (error){
+    console.log("x  An error has occurred: ", error.message)
+} finally{
+    console.log("✔ Exit of your function")
+}
 // Capturar varios tipos de errores
+try{
+    //console.log(sum(5,9.8))
+    console.log(sum("5",9))
+}catch (error){
+    if(error instanceof TypeError){
+        console.log("An error has occurred type of: ", error.message)
+    } else if(error instanceof Error){
+        console.log("An error has occurred type of: ", error.message)
+    }
+};
 
 
 
 // Crear excepciones personalizadas
 
+class MyCustomError extends Error {
+    constructor(message, a, b){
+        super(message)
+        this.a = a
+        this.b = b
+    }
+    printNumbers(){
+        console.log(`${this.a} + ${this.b}`)
+    }
+}
+try{
+    console.log(sum(0,8))
+}catch (error){
+    console.log("An personalized erro has occurred: ", error.message)
+    error.printNumbers()
 
+}
 
